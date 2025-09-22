5550dfb29d36f51e9b374fb5f7fd95fb478d1e72
konstantine.garozashvili <168521057+konstantine-garozashvili@users.noreply.github.com>
2025-09-22 16:22:12 +0200

    feat(auth): add AuthModule to lists and cards modules
    Add authentication requirement to lists and cards modules by importing AuthModule. Also include a test script to verify API endpoints including auth flow.

--- nestjs-implementation/src/lists/lists.module.ts
+++ nestjs-implementation/src/lists/lists.module.ts
@@ -4,9 +4,10 @@ import { ListsService } from './lists.service';
 import { ListsController } from './lists.controller';
 import { List } from './list.entity';
 import { ListsResolver } from './lists.resolver';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([List])],
+  imports: [TypeOrmModule.forFeature([List]), AuthModule],
   controllers: [ListsController],
   providers: [ListsService, ListsResolver],
   exports: [ListsService, TypeOrmModule],
